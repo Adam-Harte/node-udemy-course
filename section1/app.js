5e1bd19cd6a4ffc319e21356c91f56adaef35e1e
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-06 21:26:28 +0100

    body data parsed as stream data using Buffer to add rthe message text to the response file

--- section1/app.js
+++ section1/app.js
@@ -18,7 +18,19 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.writeHead(302, {
       location: "/"
     });
