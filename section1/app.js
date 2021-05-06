a03a7f9eadc2d41889c81e3ff20c9697301854a4
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-06 21:13:57 +0100

    routing and redirecting implemented

--- section1/app.js
+++ section1/app.js
@@ -1,9 +1,30 @@
 const http = require('http');
+const fs  = require ('fs');
 
 const server = http.createServer((req, res) => {
   // console.log(req);
   console.log(req.url, req.method, req.headers);
   // process.exit();
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.writeHead(302, {
+      location: "/"
+    });
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
