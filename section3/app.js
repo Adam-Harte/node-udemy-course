b7dc5e0f60e48a579032e03d241059be08776d1b
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-08 19:14:23 +0100

    route handling for different http methods added

--- section3/app.js
+++ section3/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  console.log('This always runs!');
-  next(); // Allows the response to continue to the next middleware
-});
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "add product" page!</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
