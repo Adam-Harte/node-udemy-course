4325acbfbcac2c7fa809aa57fe90f1263eff3aae
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-08 15:17:56 +0100

    route handling with express added

--- section3/app.js
+++ section3/app.js
@@ -4,10 +4,16 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('In the middleware!');
+  console.log('This always runs!');
   next(); // Allows the response to continue to the next middleware
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "add product" page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
