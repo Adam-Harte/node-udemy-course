537fc32dd2e78c62004727ed50e6f61f75bc4cb9
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-15 16:37:25 +0100

    adding products to cart and viewing cart implemented

--- section8/app.js
+++ section8/app.js
@@ -38,14 +38,14 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(product);
+User.hasMany(Product);
 User.hasOne(Cart);
-// Cart.belongsTo(User);
+Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync({ force: true })
-// sequelize.sync()
+// sequelize.sync({ force: true })
+sequelize.sync()
   .then(() => {
     return User.findByPk(1);
   })
@@ -56,7 +56,9 @@ sequelize.sync({ force: true })
     return user;
   })
   .then(user => {
-    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
