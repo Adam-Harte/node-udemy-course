a602ac3d49ee965262a294c9e7776e86d91512ee
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-16 12:21:55 +0100

    db connection finalised and Product model updated to use mongo db for creation

--- section9/util/database.js
+++ section9/util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (cb) => {
   MongoClient.connect(
-    'mongodb+srv://adamharte:QPR4life@cluster0.yjx2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'mongodb+srv://adamharte:QPR4life@cluster0.yjx2f.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      cb(client);
+      _db = client.db();
+      cb();
     })
     .catch(err => {
-      console.log(err);
+      throw err;
     });
-}
+};
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
