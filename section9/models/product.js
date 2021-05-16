a42e51f0f05aeedb0a0986b2abbfd8df00ba800f
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-16 23:11:16 +0100

    find all and find by id products implemented using mongodb

--- section9/models/product.js
+++ section9/models/product.js
@@ -1,3 +1,5 @@
+const mongodb = require('mongodb');
+
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -13,6 +15,30 @@ class Product {
     return db.collection('products').insertOne(this)
       .then(result => {
         console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products').find({ _id: new mongodb.ObjectID(prodId) }).next()
+      .then(product => {
+        console.log(product);
+        return product
       })
       .catch(err => {
         console.log(err);
