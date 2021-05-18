78576e99e285777136fbafb03dd2294508bf1e74
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-18 22:43:30 +0100

    user added with mongodb and added to the request

--- section9/models/user.js
+++ section9/models/user.js
@@ -1,16 +1,20 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-module.exports = User;
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId) {
+    db = getDb();
+    return db.collection('users').find({ _id: new mongodb.ObjectId(userId) })
+  }
+}
