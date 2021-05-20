9eb85460fcabe027b296f7da2115b0c3043cd99e
Adam-Harte <adamharte11@hotmail.co.uk>
2021-05-20 23:09:47 +0100

    express session middleware used to access session from request

--- Section11/controllers/auth.js
+++ Section11/controllers/auth.js
@@ -1,14 +1,14 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie'));
-  const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn);
+  // const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=60; HttpOnly');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 }
