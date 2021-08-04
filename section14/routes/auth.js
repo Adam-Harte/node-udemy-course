e02ffc728b48aad30612f89a294247fec8171e6a
Adam-Harte <adamharte11@hotmail.co.uk>
2021-08-04 22:25:25 +0100

    assignment 6 validation added to login

--- section14/routes/auth.js
+++ section14/routes/auth.js
@@ -9,13 +9,27 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login',
+[
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value, { req }) => {
+      return User.findOne({ email: value })
+        .then(userDoc => {
+          if (!userDoc) {
+            return Promise.reject('No user with this E-Mail exists pick a different one.');
+          }
+        });
+    }),
+    body('password', 'Please enter a password with only numbers and text and that is at least 5 characters long.').isLength({ min: 5 }).isAlphanumeric()
+], authController.postLogin);
 
 router.post('/signup',
 [
   check('email')
     .isEmail()
-    .withMessage('Please enter avalid email.')
+    .withMessage('Please enter a valid email.')
     .custom((value, { req }) => {
       // if (value === 'test@test.com') {
       //   throw new Error('This email address is forbidden.');
