28270575f9a8e9f7cd15f3df0383d7475630c4ab
Adam-Harte <adamharte11@hotmail.co.uk>
2021-08-06 00:03:18 +0100

    express error handling added to catch all cases of next that pass an error object

--- section15/controllers/admin.js
+++ section15/controllers/admin.js
@@ -70,7 +70,9 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: 'Database operation failed, please try again.',
       //   validationErrors: []
       // });
-      res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      next(error);
     });
 };
 
