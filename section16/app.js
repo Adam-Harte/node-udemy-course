76d880fc68f84fc14ad77ff393752a4fd79b5434
Adam-Harte <adamharte11@hotmail.co.uk>
2021-08-07 11:41:37 +0100

    filtering out files by mimetype added to multer config

--- section16/app.js
+++ section16/app.js
@@ -22,6 +22,14 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/png" || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images');
@@ -39,7 +47,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
